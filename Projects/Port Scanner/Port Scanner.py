89bfc5001f98c3aa64a4a033e952a6feb706d31f
TwilightsEmbrace <141727925+TwilightsEmbrace@users.noreply.github.com>
2024-07-22 12:46:10 +0100

    Add files via upload

---
+++ Projects/Port Scanner/Port Scanner.py
@@ -0,0 +1,37 @@
+import pyfiglet
+import sys
+import socket
+from datetime import datetime
+
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
+
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1])
+else:
+    print("Enter One IP Address or Website")
+
+print("-" * 50)
+print("Scanning Target: " + target)
+print("Scanning Started at: " + str(datetime.now()))
+print("-" * 50)
+
+try:
+    for port in range(1,100):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+
+        result = s.connect_ex((target,port))
+        if result ==0:
+            print("Port {} is open".format(port))
+        s.close()
+
+except KeyboardInterrupt:
+    print("\n Exitting Scan" )
+    sys.exit()
+except socket.gaierror:
+    print("\n Hostname could not be Resolved.")
+    sys.exit()
+except socket.error:
+    print("\ Server not Responding")
+    sys.exit()
---
+++ Projects/Port Scanner/README.txt
@@ -0,0 +1,2 @@
+What is a Port Scanner?
+How does a Port Scanner Work?

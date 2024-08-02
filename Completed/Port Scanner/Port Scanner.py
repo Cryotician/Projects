feb3baffc940520d47f831209acf7ec04919c75d
Cryotician <141727925+Cryotician@users.noreply.github.com>
2024-08-02 14:56:55 +0100

    Updated PortScanner

--- Completed/Port Scanner/Port Scanner.py
+++ Completed/Port Scanner/Port Scanner.py
@@ -1,37 +1,47 @@
 import pyfiglet
 import sys
 import socket
+import ipaddress
 from datetime import datetime
 
-ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
-print(ascii_banner)
+target = (input("What IP Address do you want to Scan?: "))
 
-if len(sys.argv) == 2:
-    target = socket.gethostbyname(sys.argv[1])
-else:
-    print("Enter One IP Address or Website")
+def validate_ip(target):
+    try:
+        ip_object = ipaddress.ip_address(target)
+        ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+        print(ascii_banner)
 
-print("-" * 50)
-print("Scanning Target: " + target)
-print("Scanning Started at: " + str(datetime.now()))
-print("-" * 50)
+        if len(sys.argv) == 2:
+            target = socket.gethostbyname(sys.argv[1])
+        else:
+            print("Enter One IP Address or Website")
 
-try:
-    for port in range(1,100):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
+        print("-" * 50)
+        print("Scanning Target: " + target)
+        print("Scanning Started at: " + str(datetime.now()))
+        print("-" * 50)
 
-        result = s.connect_ex((target,port))
-        if result ==0:
-            print("Port {} is open".format(port))
-        s.close()
+        try:
+            for port in range(1,100):
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                socket.setdefaulttimeout(1)
 
-except KeyboardInterrupt:
-    print("\n Exitting Scan" )
-    sys.exit()
-except socket.gaierror:
-    print("\n Hostname could not be Resolved.")
-    sys.exit()
-except socket.error:
-    print("\ Server not Responding")
-    sys.exit()
+                result = s.connect_ex((target,port))
+            if result ==0:
+                print("Port {} is open".format(port))
+                s.close()
+
+        except KeyboardInterrupt:
+            print("\n Exitting Scan" )
+            sys.exit()
+        except socket.gaierror:
+            print("\n Hostname could not be Resolved.")
+            sys.exit()
+        except socket.error:
+            print("\ Server not Responding")
+            sys.exit()
+    except ValueError:
+        print("Invalid IP Address Provided")
+        
+validate_ip(target)
